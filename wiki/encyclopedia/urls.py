8f44cba7ae5fcb78da40db2484bb2633a568f952
MudkipMao <splatt9198@gmail.com>
2021-01-13 15:55:27 -0500

    Added random functionality, and have searching logic done. Todo: add search result view

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("random", views.random, name="random"),
-    path("<str:article_name>", views.article, name="article")
+    path("random/", views.random, name="random"),
+    path("wiki/<str:article_name>", views.article, name="article"),
+    path("search", views.search, name="search")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from markdown2 import Markdown
+from django import forms
 
 from . import util
-from random import randrange
+from random import choice
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(label="q")
 
 
 def getHTMLfromMarkdown(article_markdown):
@@ -33,12 +39,15 @@ def article(request, article_name):
 
 def random(request):
     all_articles = util.list_entries()
-    num_range = len(all_articles)
-    chosen_num = randrange(num_range)
-    article_name = all_articles[chosen_num]
-    article_markdown = util.get_entry(article_name)
-    return render(request, "encyclopedia/article.html", {
-        "article_name": article_name,
-        "article_body": getHTMLfromMarkdown(article_markdown)
-    })
+    selected_page = choice(all_articles)
+    return redirect('encyclopedia:article', article_name=selected_page)
 
+
+def search(request):
+    query = request.GET.get('q').lower()
+    all_articles = [x.lower() for x in util.list_entries()]
+    if query in all_articles:
+        return redirect('encyclopedia:article', article_name=query)
+
+    all_articles = [x for x in all_articles if query in x]
+    return HttpResponse(f"matches are {all_articles}")
